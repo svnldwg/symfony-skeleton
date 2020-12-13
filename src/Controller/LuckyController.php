a77e12406836029ac223b40d9b07386b956a04b3
svnldwg <s.ludwig.g@gmail.com>
2020-12-13 23:32:05 +0100

    adds first twig template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
      * @Route("/lucky")
@@ -13,8 +15,8 @@ class LuckyController
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
