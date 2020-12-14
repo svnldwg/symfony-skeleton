048a2d9df45e1715bf719578d5b2278cd3eaa815
svnldwg <s.ludwig.g@gmail.com>
2020-12-15 00:26:11 +0100

    persist generated number

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\NumberStore;
 use App\Service\NumberGenerator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/lucky/{max}")
      */
     public function number(int $max, NumberGenerator $numberGenerator): Response
     {
         $number = $numberGenerator->random(0, $max);
+
+        $numberEntity = new NumberStore($number);
+        $this->entityManager->persist($numberEntity);
+        $this->entityManager->flush();
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
